fc9eedfe997ca8830dd41adf82b87a4106935039
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2014-05-07 14:21:25 -0700

    Added support for descendant taxon queries.
    It's still not particularly fast, but possibly fast enough w/ caching.
    The main finding here is that hard coding the id and ancestry into the
    query seems to make better use of the indices than joining in those
    values from the taxa table.
    
    I've also started using Step to control flow, since it seemed
    necessary when working with the node-postgres library.

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var _         = require('underscore')
 var one       = require('onecolor')
 var squel     = require('squel')
 var conf      = require('./config')
+var Step       = require('step')
+var pg = require('pg')
+var pgConnString = 
+  "postgres://"+conf.database.user+
+  "@"+conf.database.host+
+  ":"+conf.database.port+
+  "/"+conf.database.database_name
 
 squel.useFlavour('postgres')
 
@@ -91,12 +98,12 @@ var defaultStyleGrid =
   " text-allow-overlap: false;" +
   "}" +*/
   "[cnt>=25] { polygon-fill: {{color}}; polygon-opacity:1.0;  } " +
-  "[cnt<20]  { polygon-fill: {{color}}; polygon-opacity:0.8;  } " +
-  "[cnt<15]  { polygon-fill: {{color}}; polygon-opacity:0.6;  } " +
-  "[cnt<10]  { polygon-fill: {{color}}; polygon-opacity:0.4;  } " +
-  "[cnt<5]  { polygon-fill: {{color}}; polygon-opacity:0.2;  } }";
+  "[cnt<25]  { polygon-fill: {{color}}; polygon-opacity:0.9;  } " +
+  "[cnt<15]  { polygon-fill: {{color}}; polygon-opacity:0.8;  } " +
+  "[cnt<10]  { polygon-fill: {{color}}; polygon-opacity:0.7;  } " +
+  "[cnt<5]  { polygon-fill: {{color}}; polygon-opacity:0.6;  } }";
 
-function gridRequest(req) {
+function gridRequest(req, callback) {
   var seed = 16/Math.pow(2,parseInt(req.params.z));
   if (seed > 4) {
     seed = 4;
@@ -105,11 +112,16 @@ function gridRequest(req) {
   }
   var gq = gridQuery.clone(),
       sq = gridSnapQuery.clone()
-  if (req.params.taxon_id) {
+  if (req.inat && req.inat.taxon) {
     gq.field('taxon_id')
-    sq.field('taxon_id')
-    sq.where('taxon_id = '+req.params.taxon_id)
-    sq.group('taxon_id')
+    sq.field('o.taxon_id')
+    sq
+      .join('taxa t', null, 't.id = o.taxon_id')
+      .where(
+        't.id = '+req.inat.taxon.id+ 
+        " OR t.ancestry = '" + req.inat.taxon.child_ancestry + "'" +
+        " OR t.ancestry LIKE '" + req.inat.taxon.descendant_ancestry + "'")
+    sq.group('o.taxon_id')
   }
   gq.from('('+sq.toString()+') AS snap_grid')
   req.params.sql = '('+gq.toString()+') AS obs_grid'
@@ -122,28 +134,31 @@ function gridRequest(req) {
   } else {
     req.params.style = req.params.style.replace(/\{\{color\}\}/g,'#333333')
   }
-  return req
+  callback(null, req)
 }
 
-function pointsRequest(req) {
+function pointsRequest(req, callback) {
   if (!req.params.style) {
     req.params.style = defaultStylePoints;
   }
-  req = commonPointRequest(req, pointQuery.clone())
-  return req
+  req = commonPointRequest(req, pointQuery.clone(), callback)
 }
 
-function pointPrecisionsRequest(req) {
+function pointPrecisionsRequest(req, callback) {
   if (!req.params.style) {
     req.params.style = pointPrecisionStyle;
   }
-  req = commonPointRequest(req, pointPrecisionQuery.clone())
-  return req
+  req = commonPointRequest(req, pointPrecisionQuery.clone(), callback)
 }
 
-function commonPointRequest(req, query) {
-  if (req.params.taxon_id) {
-    query.where("o.taxon_id = "+req.params.taxon_id)
+function commonPointRequest(req, query, callback) {
+  if (req.inat && req.inat.taxon) {
+    query
+      .join('taxa t', null, 't.id = o.taxon_id')
+      .where(
+        't.id = '+req.inat.taxon.id+ 
+        " OR t.ancestry = '" + req.inat.taxon.child_ancestry + "'" +
+        " OR t.ancestry LIKE '" + req.inat.taxon.descendant_ancestry + "'")
   }
   if (req.params.obs_id) {
     query.where("o.id != "+req.params.obs_id)
@@ -158,10 +173,10 @@ function commonPointRequest(req, query) {
     req.params.style = req.params.style.replace(/\{\{color\}\}/g,'#333333');
     req.params.style = req.params.style.replace(/\{\{outline_color\}\}/g,'#AAAAAA')
   }
-  return req
+  callback(null, req)
 }
 
-function timelineRequest(req) {
+function timelineRequest(req, callback) {
   /*req.params.sql = "(SELECT id, observed_on, species_guess, iconic_taxon_id, taxon_id, latitude, longitude, geom, " +
     "positional_accuracy, captive, quality_grade FROM " +
     "observations o WHERE observed_on <= TO_DATE('{{date_up}}','YYYY-MM-DD')) as observations";*/
@@ -189,7 +204,42 @@ function timelineRequest(req) {
     req.params.style = req.params.style.replace(/\{\{color\}\}/g,req.params.color);
   }
   req.params.interactivity="id";
-  return req
+  callback(null, req)
+}
+
+var loadTaxon = function(req, callback) {
+  var taxonId = req.params.taxon_id
+  Step(
+    function() {
+      pg.connect(pgConnString, this)
+    },
+    function handleConnection(err, client, done) {
+      if (err) {
+        console.error('could not connect to postgres', err)
+        callback(null, req)
+        return null
+      }
+      client.query('SELECT id, name, ancestry, rank, rank_level FROM taxa WHERE id = '+taxonId, this)
+      done()
+    },
+    function handleResult(err, result) {
+      if (err) console.error('error running query', err)
+      if (result.rows.length > 0) {
+        var ancestry = result.rows[0].ancestry
+        req.inat = req.inat || {}
+        req.inat.taxon = {
+          id: result.rows[0].id,
+          name: result.rows[0].name,
+          rank: result.rows[0].rank,
+          rankLevel: result.rows[0].rank_level,
+          ancestry: ancestry,
+          child_ancestry: ancestry + '/' + taxonId,
+          descendant_ancestry: ancestry + '/' + taxonId + '/%'
+        }
+      }
+      callback(null, req)
+    }
+  )
 }
 
 var config = {
@@ -213,42 +263,61 @@ var config = {
 
     req.params.dbname = conf.database.database_name;
 
-    // in lieu of a proper router
-    if (req.params.endpoint == 'grid') {		//Grid endpoint
-      req = gridRequest(req)    
-    } else if (req.params.endpoint == 'points') {	// Points endpoint
-      req = pointsRequest(req)
-    } else if (req.params.endpoint == 'precisions') {
-      req = pointPrecisionsRequest(req)
-    } else if (req.params.endpoint == 'timeline') {
-      req = timelineRequest(req)
-    } else {
-      // just a precaution to try and prevent sql injection
-      req.params.sql = "WHERE 1 = 2"
-    }
+    Step(
+      function loadTaxonForTaxonFilter() {
+        if (req.params.taxon_id) {
+          loadTaxon(req, this)
+        } else {
+          return req
+        }
+      },
+      function processRequestForEndpoint(err, req) {
+        if (err) {
+          console.log("[DEBUG] failed to get ancestries")
+        }
+        // in lieu of a proper router
+        if (req.params.endpoint == 'grid') {    //Grid endpoint
+          gridRequest(req, this)
+        } else if (req.params.endpoint == 'points') { // Points endpoint
+          pointsRequest(req, this)
+        } else if (req.params.endpoint == 'precisions') {
+          pointPrecisionsRequest(req, this)
+        } else if (req.params.endpoint == 'timeline') {
+          timelineRequest(req, this)
+        } else {
+          // just a precaution to try and prevent sql injection
+          req.params.sql = "(SELECT geom FROM observations WHERE 1 = 2) AS foo"
+          return req
+        }
+      },
+      function finalize(err, req) {
+        if (err) {
+          console.log("[DEBUG] failed to process request for endpoint, err: " + err)
+        }
+        // send the finished req object on
+        var x = parseInt(req.params.x),
+            y = parseInt(req.params.y),
+            z = parseInt(req.params.z),
+            numTiles = Math.pow(2,z),
+            maxCoord = numTiles - 1
+        
+        x = x >= 0 ? x : maxCoord + x
+        y = y >= 0 ? y : maxCoord + y
+        if (x > maxCoord) {x = x % numTiles}
+        if (y > maxCoord) {y = y % numTiles}
+        if (x < -1*maxCoord) {x = Math.abs(x) % numTiles}
+        if (y < -1*maxCoord) {y = Math.abs(y) % numTiles}
+        req.params.x = ''+x
+        req.params.y = ''+y
+        req.params.z = ''+z
 
-    // send the finished req object on
-    var x = parseInt(req.params.x),
-        y = parseInt(req.params.y),
-        z = parseInt(req.params.z),
-        numTiles = Math.pow(2,z),
-        maxCoord = numTiles - 1
-    
-    x = x >= 0 ? x : maxCoord + x
-    y = y >= 0 ? y : maxCoord + y
-    if (x > maxCoord) {x = x % numTiles}
-    if (y > maxCoord) {y = y % numTiles}
-    if (x < -1*maxCoord) {x = Math.abs(x) % numTiles}
-    if (y < -1*maxCoord) {y = Math.abs(y) % numTiles}
-    req.params.x = ''+x
-    req.params.y = ''+y
-    req.params.z = ''+z
+        if (conf.debug) {
+          console.log("[DEBUG] req.params.sql: ", req.params.sql)
+        }
 
-    if (conf.debug) {
-      console.log("[DEBUG] req.params.sql: ", req.params.sql)
-    }
-
-    callback(null,req);
+        callback(null,req);
+      }
+    )
   },
   beforeTileRender: function(req, res, callback) {
     callback(null);
