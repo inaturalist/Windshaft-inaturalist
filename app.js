a417ed48d416802b95158e9428492609a50ef8e0
Patrick Leary <patrick.r.leary@gmail.com>
2015-03-11 17:41:11 -0400

    Updated newrelic.js path; updated example config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var conf = require( "./config" );
 var fs = require( "fs" );
+var path = require( "path" );
 // require and configure nodetime if configured to do so
 // Nodetime is a application stats and performance monitor
 if( conf.nodetime && conf.nodetime.accountKey ) {
@@ -9,7 +10,7 @@ if( conf.nodetime && conf.nodetime.accountKey ) {
   });
 }
 // NewRelic is another stats module, which needs newrelic.js in the app root
-if( fs.existsSync("newrelic.js") ) {
+if( fs.existsSync( path.join(path.dirname(fs.realpathSync(__filename)), "newrelic.js") ) ) {
   var newrelic = require( "newrelic" );
 }
 var inaturalist = require( "./lib/inaturalist" );
